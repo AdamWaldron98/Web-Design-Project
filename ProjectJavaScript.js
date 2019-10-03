c56cc85e0b51b6a07ac8fcc37024c4d6553646eb
Adam Waldron <44497139+AdamWaldron98@users.noreply.github.com>
2019-10-03 19:21:15 +0100

    Add files to github

---
+++ ProjectJavaScript.js
@@ -0,0 +1,23 @@
+//Function for maths addition answers
+function addition()
+{
+	document.getElementById("answers").innerHTML = "1. 9 \\ 2. 13 \\ 3. 16 \\ 4. 20 \\ 5. 22 ";
+}
+
+//Function for maths subtraction answers
+function subtraction()
+{
+	document.getElementById("answers1").innerHTML = "1. 2 \\ 2. 3 \\ 3. 7 \\ 4. 8 \\ 5. 9 ";
+}
+
+//Function for maths multiplication answers
+function multiplication()
+{
+	document.getElementById("answers2").innerHTML = "1. 4 \\ 2. 9 \\ 3. 12 \\ 4. 3 \\ 5. 20 ";
+}
+
+//Function for maths division answers
+function division()
+{
+	document.getElementById("answers3").innerHTML = "1. 2 \\ 2. 3 \\ 3. 4 \\ 4. 1 \\ 5. 5 ";
+}
